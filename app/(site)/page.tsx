39b0d926505c00154f4d5cbdb6eb98c37f4504e6
Angga Ari Wijaya <anggadarkprince@gmail.com>
2023-09-19 20:29:07 +0700

    Initial commit from Create Next App

---
+++ app/(site)/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <p className="text-sky-500 text-3xl">ChatWave</p>
+      </div>
+    </div>
+  )
+}
