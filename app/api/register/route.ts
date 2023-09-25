888b3bf434914bf8bc4a970ec8486e8bfba79a27
Angga Ari Wijaya <anggadarkprince@gmail.com>
2023-09-25 15:22:43 +0700

    feat(layout): add sidebar

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -7,7 +7,7 @@ export async function POST(request: Request) {
     const body = await request.json();
     const {email, name, password} = body;
     if (!email || !name || !password) {
-      return new NextResponse('Missing info', {status: 400});
+      return new NextResponse('Missing user data', {status: 400});
     }
 
     const hashedPassword = await bcrypt.hash(password, 12);
